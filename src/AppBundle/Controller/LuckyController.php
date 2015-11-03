cd827032adddcef3ee82bb20f5661a6a59f0f484
James Golz <jgolz@laserspineinstitute.com>
2015-11-03 14:33:49 -0500

    next bit in tutorial - add json response endpoint to babby controller

--- src/AppBundle/Controller/LuckyController.php
+++ src/AppBundle/Controller/LuckyController.php
@@ -11,17 +11,24 @@ class LuckyController extends Controller {
 	/**
      * @Route("/lucky/")
      */
-    public function indexAction()
-    {
+    public function indexAction(){
 		return new Response("<html><body>IT WORKS!</body></html>");
 	}
 	
 	/**
-	* @route("/lucky/number")
+	* @Route("/lucky/number")
 	*/
 	public function numberAction(){
 		$number = rand(0, 100);		
 		return new Response("<html><body>Lucky number: ".$number."</body></html>");
+	}
+	
+	/**
+	* @Route("/api/lucky/number")
+	*/
+	public function apiNumberAction(){
+		$data = array('lucky_number' => rand(0, 100));
+		return new Response(json_encode($data), 200, array("Content-Type" => 'application/json'));
 	}
 	
 }
